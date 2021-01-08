66d01025ee7cb694d9c6cb59773de255d48e9779
Rucha Deshpande <deshruch@amazon.com>
2021-01-08 17:08:48 +0000

    Update buildspec.yaml for docker commit a7f50fe1 Author: Rucha Deshpande <deshruch@amazon.com> Date: Fri Jan 08 2021 10:38:48 GMT-0600 (Central Standard Time)
        Merge branch 'dev' into deshruch
    
    
    commit fac002e3 
    Author: Rucha Deshpande <deshruch@amazon.com> 
    Date: Fri Jan 08 2021 10:34:42 GMT-0600 (Central Standard Time) 
    
        fix copyright
    
    
    commit 09f9c148 
    Author: Rucha Deshpande <deshruch@amazon.com> 
    Date: Thu Jan 07 2021 16:42:31 GMT-0600 (Central Standard Time) 
    
        fix image
    
    
    commit 78b16a71 
    Author: Rucha Deshpande <deshruch@amazon.com> 
    Date: Thu Jan 07 2021 16:36:48 GMT-0600 (Central Standard Time) 
    
        Add dockerfile
    
    
    commit abb6647b 
    Author: Rucha Deshpande <deshruch@amazon.com> 
    Date: Thu Jan 07 2021 15:01:55 GMT-0600 (Central Standard Time) 
    
        Merge branch 'deshruch' of codecommit://os-seismic-store-service into deshruch
    
    
    commit a1b1edde 
    Author: Rucha Deshpande <deshruch@amazon.com> 
    Date: Thu Jan 07 2021 15:01:25 GMT-0600 (Central Standard Time) 
    
        Add config and index file
    
    
    commit 2ef08d05 
    Author: Rucha Deshpande <deshruch@amazon.com> 
    Date: Thu Jan 07 2021 13:20:32 GMT-0600 (Central Standard Time) 
    
        Remove comments
    
    commit 8109fecb 
    Author: Rucha Deshpande <deshruch@amazon.com> 
    Date: Thu Jan 07 2021 11:43:13 GMT-0600 (Central Standard Time) 
    
        Add buildspec for aws
    
    
    commit 5cf37ccd 
    Author: Rucha Deshpande <deshruch@amazon.com> 
    Date: Thu Jan 07 2021 11:06:43 GMT-0600 (Central Standard Time) 
    
        add config.ts
    
    
    commit 5409e041 
    Author: Rucha Deshpande <deshruch@amazon.com> 
    Date: Thu Jan 07 2021 11:02:51 GMT-0600 (Central Standard Time) 
    
        Initial AWS commit

--- src/cloud/providers/aws/config.ts
+++ src/cloud/providers/aws/config.ts
@@ -11,3 +11,62 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
+import { Config, ConfigFactory } from '../../config';
+@ConfigFactory.register('aws')
+export class AWSConfig extends Config {
+    // scopes
+    public static AWS_EP_OAUTH2: string;
+    public static AWS_EP_IAM: string;
+    public static AWS_REGION: string;
+
+    // max len for a group name in DE
+    public static DES_GROUP_CHAR_LIMIT = 256;
+
+    public async init(): Promise<void> {
+
+        // init AWS specific configurations
+        AWSConfig.AWS_EP_OAUTH2 = process.env.WS_EP_OAUTH2;
+        AWSConfig.AWS_EP_IAM = process.env.AWS_EP_IAM;
+        AWSConfig.AWS_REGION = process.env.AWS_REGION;
+
+        Config.initServiceConfiguration({
+            SERVICE_ENV: process.env.SERVICE_ENV,
+            SERVICE_PORT: +process.env.PORT || 5000,
+            API_BASE_PATH: process.env.API_BASE_PATH,
+            IMP_SERVICE_ACCOUNT_SIGNER: process.env.IMP_SERVICE_ACCOUNT_SIGNER,
+            LOCKSMAP_REDIS_INSTANCE_ADDRESS: process.env.LOCKSMAP_REDIS_INSTANCE_ADDRESS,
+            LOCKSMAP_REDIS_INSTANCE_PORT: +process.env.LOCKSMAP_REDIS_INSTANCE_PORT,
+            LOCKSMAP_REDIS_INSTANCE_KEY: process.env.LOCKSMAP_REDIS_INSTANCE_KEY,
+            DES_REDIS_INSTANCE_ADDRESS: process.env.DES_REDIS_INSTANCE_ADDRESS,
+            DES_REDIS_INSTANCE_PORT: +process.env.DES_REDIS_INSTANCE_PORT,
+            DES_REDIS_INSTANCE_KEY: process.env.DES_REDIS_INSTANCE_KEY,
+            DES_SERVICE_HOST: process.env.DES_SERVICE_HOST,
+            DES_SERVICE_APPKEY: process.env.DES_SERVICE_APPKEY,
+            DES_GROUP_CHAR_LIMIT: AWSConfig.DES_GROUP_CHAR_LIMIT,
+            JWKS_URL: process.env.JWKS_URL,
+            JWT_EXCLUDE_PATHS: process.env.JWT_EXCLUDE_PATHS,
+            JWT_AUDIENCE: process.env.JWT_AUDIENCE,
+            JWT_ENABLE_FEATURE: process.env.JWT_ENABLE_FEATURE ? process.env.JWT_ENABLE_FEATURE === 'true' : false,
+            TENANT_JOURNAL_ON_DATA_PARTITION: false,
+            FEATURE_FLAG_AUTHORIZATION: process.env.FEATURE_FLAG_AUTHORIZATION !== undefined ?
+                process.env.FEATURE_FLAG_AUTHORIZATION !== 'false' : true,
+            FEATURE_FLAG_LEGALTAG: process.env.FEATURE_FLAG_LEGALTAG !== undefined ?
+                process.env.FEATURE_FLAG_LEGALTAG !== 'false' : true,
+            FEATURE_FLAG_SEISMICMETA_STORAGE: process.env.FEATURE_FLAG_SEISMICMETA_STORAGE !== undefined ?
+                process.env.FEATURE_FLAG_SEISMICMETA_STORAGE !== 'false' : true,
+            FEATURE_FLAG_IMPTOKEN: process.env.FEATURE_FLAG_IMPTOKEN !== undefined ?
+                process.env.FEATURE_FLAG_IMPTOKEN !== 'false' : true,
+            FEATURE_FLAG_STORAGE_CREDENTIALS: process.env.FEATURE_FLAG_STORAGE_CREDENTIALS !== undefined ?
+                process.env.FEATURE_FLAG_STORAGE_CREDENTIALS !== 'false' : true,
+            FEATURE_FLAG_TRACE: process.env.FEATURE_FLAG_TRACE !== undefined ?
+                process.env.FEATURE_FLAG_TRACE !== 'false' : true,
+            FEATURE_FLAG_LOGGING: process.env.FEATURE_FLAG_LOGGING !== undefined ?
+                process.env.FEATURE_FLAG_LOGGING !== 'false' : true,
+            FEATURE_FLAG_STACKDRIVER_EXPORTER: process.env.FEATURE_FLAG_STACKDRIVER_EXPORTER !== undefined ?
+                process.env.FEATURE_FLAG_STACKDRIVER_EXPORTER !== 'false' : true
+        });
+
+    }
+
+}
