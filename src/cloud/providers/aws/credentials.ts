6d19ed38540fe3dee927c6966e9aa5b0f0a3d738
Rucha Deshpande <deshruch@amazon.com>
2021-01-21 20:59:43 +0000

    Credentials API implementation
    commit 17c1f924
    Author: Rucha Deshpande <deshruch@amazon.com>
    Date: Thu Jan 21 2021 14:18:02 GMT-0600 (Central Standard Time)
    
        Merge branch 'dev' into deshruch
    
    commit 30b0d577
    Author: Rucha Deshpande <deshruch@amazon.com>
    Date: Thu Jan 21 2021 14:15:21 GMT-0600 (Central Standard Time)
    
        updates
    
    commit 701d6a26
    Author: Rucha Deshpande <deshruch@amazon.com>
    Date: Thu Jan 21 2021 11:46:35 GMT-0600 (Central Standard Time)
    
        remove TEMP_CREDENTIALS_EXP_DURATION from env
    
    commit 18f2bba1
    Author: Rucha Deshpande <deshruch@amazon.com>
    Date: Thu Jan 21 2021 11:44:16 GMT-0600 (Central Standard Time)
    
        Add exp duration as SSM parameter
    
    commit b00dacae
    Author: Rucha Deshpande <deshruch@amazon.com>
    Date: Thu Jan 21 2021 11:34:43 GMT-0600 (Central Standard Time)
    
        Add credentials API
    
    commit 1b01944d
    Author: Rucha Deshpande <deshruch@amazon.com>
    Date: Thu Jan 21 2021 10:50:35 GMT-0600 (Central Standard Time)
    
        Implement credentials API
    
    commit bbceae8f
    Author: Rucha Deshpande <deshruch@amazon.com>
    Date: Wed Jan 20 2021 14:25:48 GMT-0600 (Central Standard Time)
    
        Added ssm and sts helper
    
    commit 30b59346
    Author: Rucha Deshpande <deshruch@amazon.com>
    Date: Tue Jan 19 2021 10:05:31 GMT-0600 (Central Standard Time)
    
        Merge branch 'dev' into deshruch
    
    commit 0c6b62ec
    Author: Rucha Deshpande <deshruch@amazon.com>
    Date: Fri Jan 15 2021 16:49:04 GMT-0600 (Central Standard Time)
    
        Merge branch 'dev' into deshruch
    
    commit 084954a9
    Author: Rucha Deshpande <deshruch@amazon.com>
    Date: Fri Jan 15 2021 16:47:11 GMT-0600 (Central Standard Time)
    
        Bug Fix: correct DES service paths
    
    commit 916afd66
    Author: Rucha Deshpande <deshruch@amazon.com>
    Date: Fri Jan 15 2021 16:00:03 GMT-0600 (Central Standard Time)
    
        Add skeletons for provider interfaces

--- src/cloud/providers/aws/credentials.ts
+++ src/cloud/providers/aws/credentials.ts
@@ -16,9 +16,16 @@ import { Config } from '../../../cloud';
 import { Error, Utils } from '../../../shared';
 import { AbstractCredentials, CredentialsFactory, IAccessTokenModel } from '../../credentials';
 import { AWSConfig } from './config';
+import {AWSSSMhelper} from './ssmhelper';
+import {AWSSTShelper} from './stshelper';
+
 
 @CredentialsFactory.register('aws')
 export class AWSCredentials extends AbstractCredentials {
+
+    private awsSSMHelper = new AWSSSMhelper();
+    private awsSTSHelper = new AWSSTShelper();
+
     getAudienceForImpCredentials(): string {
         return '';
     }
@@ -36,7 +43,39 @@ export class AWSCredentials extends AbstractCredentials {
     }
 
     async getUserCredentials(subject: string): Promise<IAccessTokenModel> {
-        return undefined;
+        //subject = tenantName:subprojectName:1 ==> readOnly true
+        //subject = tenantName:subprojectName:0 ==> readOnly false
+        const s3bucket = await this.awsSSMHelper.getSSMParameter('/osdu/'+AWSConfig.AWS_ENVIRONMENT+'/seismic-store/seismic-s3-bucket-name')
+        const expDuration = await this.awsSSMHelper.getSSMParameter('/osdu/'+AWSConfig.AWS_ENVIRONMENT+'/seismic-store/temp-cred-expiration-duration')
+        const vars = subject.split(':')
+        const tenant = vars[0];
+        const subproject = vars[1];
+        const readOnly = vars[2];
+        var roleArn='';
+        var credentials='';
+
+        var flagUpload=true;
+
+        const keypath = tenant+'/'+subproject;
+        // tslint:disable-next-line:triple-equals
+        if(readOnly ==='1') { // readOnly True
+             roleArn = await this.awsSSMHelper.getSSMParameter('/osdu/' + AWSConfig.AWS_ENVIRONMENT + '/seismic-store/iam/download-role-arn')
+            flagUpload = false;
+        } else if (readOnly ==='0')  // readOnly False
+        {
+            roleArn = await this.awsSSMHelper.getSSMParameter('/osdu/' + AWSConfig.AWS_ENVIRONMENT + '/seismic-store/iam/upload-role-arn')
+            flagUpload = true;
+        }
+
+        credentials = await this.awsSTSHelper.getCredentials(s3bucket,keypath,roleArn,flagUpload,expDuration);
+
+
+            const result = {
+            access_token: credentials,
+            expires_in: 3599,
+            token_type: 'STSToken',
+        };
+        return result;
     }
 
     // this will return serviceprincipal access token
