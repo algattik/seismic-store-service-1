17b62b0180b7971cb88c31af9c17cd756e14d02d
Daniel Perez <dperez50@slb.com>
2021-04-06 10:53:50 -0500

    Removing top bar

--- src/server/server.ts
+++ src/server/server.ts
@@ -78,7 +78,9 @@ export class Server {
         this.app.disable('x-powered-by');
         this.app.use(cors(this.corsOptions));
         this.app.options('*', cors());
-        this.app.use('/seistore-svc/api/v3/swagger-ui.html',swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+        this.app.use('/seistore-svc/api/v3/swagger-ui.html',swaggerUi.serve, swaggerUi.setup(swaggerDocument,{
+            customCss: '.swagger-ui .topbar { display: none }'
+          }));
         this.app.use((req: express.Request, res: express.Response, next: express.NextFunction) => {
 
             // not required anymore - to verify
