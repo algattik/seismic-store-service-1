9570b2ebaf5ffaddbcb091b5bc2bc3910a6ede73
Varunkumar Manohar <vmanohar@slb.com>
2021-09-20 17:21:14 +0000

    feat: enforce conventional commits for every update and  fix husky hook execution

---
+++ app/sdms/commitlint.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+	extends: ["@commitlint/config-conventional"],
+	helpUrl: "Commits need to follow coventional commits syntax https://www.conventionalcommits.org/en/v1.0.0/#summary",
+};
